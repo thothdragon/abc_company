dee9a6d0d26ba7550fe9baeade89fefb9aee6786
Thomas Guilloux <thothdragons@gmail.com>
2022-08-23 21:01:26 +0200

    Initialize project
    Typescript, React, Redux

---
+++ abccompany/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+import './App.css';
+
+function App() {
+  return (
+    <Box>
+
+      <Typography>
+        Hello World !
+      </Typography>
+
+    </Box>
+  );
+}
+
+export default App;
---
+++ abccompany/src/redux/store.ts
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+
+const middleware = [thunk];
+
+const reducer = combineReducers({
+
+});
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(
+        applyMiddleware(
+            ...middleware
+        )
+    )
+);
+
+export default store;
